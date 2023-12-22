d87d3db7945c2ac28aa2df07ff2f89135e321188
Youki <nucacola8@gmail.com>
2023-12-22 23:56:37 +0900

    Forgot to push

--- cpu.py
+++ cpu.py
@@ -10,7 +10,8 @@ class Cpu:
         self.resister = [ [0] for i in range(num_resister)]
         self.cache = Cache()
         self.memory = Memory()
-       
+       # cache flag
+
     def set_counter(self, value = 1):
         self.cpu_counter += value
 
@@ -23,7 +24,8 @@ class Cpu:
     def reset_resister(self):
         for i in range(len(self.resister)):
             self.resister[i] = 0
-    
+
+# review write and replace policies, addresses are bins    
     def search_cache(self, address):
         self.cache.search_value(address)
 
@@ -36,3 +38,30 @@ class Cpu:
     def write_memory(self, address, value):
         self.cache.write_memory(address, value)
 
+# orders at each instruction
+    def add(self, rd, rs, rt):
+        self.resister[rd] = self.resister[rs] + self.resister[rt]
+
+    def addi(self, rd, rs, immediate):
+        self.resister[rd] = self.resister[rs] + int(immediate)
+
+
+
+
+
+
+# main parse function
+    def parse_instruction(self, instruction):
+        instruction_parsed = instruction.split(",")
+        print("Reading instruction: " + instruction)
+        self.increment_cpu_counter()
+        if instruction_parsed[0] == ADD_INSTRUCTION_OPERATOR:
+            self.add_instruction(instruction_parsed[1], instruction_parsed[2], instruction_parsed[3])
+        if instruction_parsed[0] == ADD_I_INSTRUCTION_OPERATOR:
+            self.add_i_instruction(instruction_parsed[1], instruction_parsed[2], instruction_parsed[3])
+        if instruction_parsed[0] == JUMP_INSTRUCTION_OPERATOR:
+            self.jump_instruction(instruction_parsed[1])
+        if instruction_parsed[0] == CACHE_INSTRUCTION_OPERATOR:
+            self.cache_instruction(instruction_parsed[1])
+
+            
